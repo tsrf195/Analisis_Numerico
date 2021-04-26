83d0593f31cb96c844823b1b8c4096d4c04aa42e
tsrf195 <42554436+tsrf195@users.noreply.github.com>
2021-04-26 08:12:42 -0500

    Add files via upload

---
+++ Talleres/TallerPunto13_2_grado.R
@@ -0,0 +1,51 @@
+#Lagrange
+library(rSymPy)
+library(polynom)
+
+x <- c(4410000,4830000,5250000)
+y <- c(1165978,1329190,1501477)
+
+plot(x ,y , type = "o", main="Datos",xlab = "Base Imponible", ylab = "Cuota Integra")
+
+lagrange.poly <- function(x, y) {
+  
+  l <- list()
+  k <- 1
+  
+  for (i in x) {
+    num <- 1
+    denom <- 1
+    
+    p <- x[! x %in% i]
+    
+    for (j in p) {
+      num <- paste(num, "*", "(", 'x', " - ", as.character(j), ")", sep = "", collapse = "")
+      denom <- paste(denom, "*", "(", as.character(i)," - ", as.character(j), ")", sep = "", collapse = "")
+    }
+    
+    l[k] <- paste("(", num, ")", "/", "(", denom, ")", sep = "", collapse = "")
+    k <- k + 1
+  }
+  
+  eq <- 0
+  
+  for (i in 1:length(y)) {
+    eq <- paste(eq, '+', as.character(y[i]), "*", l[[i]], sep = "", collapse = "")
+  }
+  
+  x <- Var('x')
+  
+  return(sympy(paste("simplify(", eq, ")")))
+}
+
+lagrange.poly(x, y)
+poly.calc(x, y)
+
+f <- function(x) {
+  return(2.572279e-08*x^2 + 0.1509214*x + 155.125)
+}
+plot(f, type = "l", main="Comportamiento polinomio de Lagrage")
+
+155.125 + 0.1509214*(5000000) + 2.572279e-08*(5000000)^2
+
+
